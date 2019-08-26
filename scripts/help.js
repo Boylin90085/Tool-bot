9723448d5e84c2e2ad56b222e280dfbaad1e38b1
Rick Lin <ricklin@ctwant.com>
2019-08-26 15:28:03 +0800

    新增 meme

--- scripts/help.js
+++ scripts/help.js
@@ -1,11 +1,11 @@
 module.exports = function(bot) {
-  bot.hear(/你會做什麼/, (res) => {
+  bot.hear(/貓貓會做什麼/, (res) => {
     const cmd = [
       '飲料菜單 店名(目前有: 五十嵐, 珍煮丹, 迷客夏)'
     ]
 
     cmd.forEach((c) => {
-      res.send(c)
+      res.send(c);
     })
-  })
+  });
 }
