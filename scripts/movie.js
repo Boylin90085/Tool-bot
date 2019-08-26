f38951f80afd9db518215678abfabbc016a92c27
Rick Lin <ricklin@ctwant.com>
2019-08-26 16:29:56 +0800

    更新 movie

--- scripts/movie.js
+++ scripts/movie.js
@@ -2,20 +2,24 @@ const cheerio = require("cheerio");
 const HOST = 'http://www.atmovies.com.tw/';
 
 module.exports = function(bot) {
-  bot.hear(/最近的電影/, (res) => {
-    const target_url = "http://www.atmovies.com.tw/movie/next/";
+  bot.hear(/上映中的電影/, (res) => {
+    // const target_url = "http://www.atmovies.com.tw/movie/next/";
+    const target_url = 'https://movies.yahoo.com.tw/movie_intheaters.html';
     // err錯誤訊息, res回應頭, body回應內容
     bot.http(target_url).get()((err, response, body) => {
       if (response.statusCode !== 200) {
-				return res.send("開眼電影網站異常");
+				return res.send("Yahoo 電影網站異常");
       }
-      var $ = cheerio.load(body);
-      var movieList = $('div.filmtitle > a')
+      const $ = cheerio.load(body);
+      const movieList = $('.release_list > li .release_info .release_movie_name > a');
       // console.log(movieList.text())
-      movieList.each((i, el) => {
-        var title = $(el).text();
-        var url = HOST + $(el).attr('href');
-        res.send(title+url);
+      if (movieList.length > 10) {
+        movieList = movieList.splice(0, 9)
+      }
+      $(movieList).each((i, el) => {
+        const title = $(el).text().trim();
+        const url = $(el).attr('href');
+        res.send(title+' '+url);
       });
     })
   })
