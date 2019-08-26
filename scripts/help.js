2b382899d505e4fe76c221980ed35296a7ade1b4
Rick Lin <ricklin@ctwant.com>
2019-08-26 15:17:21 +0800

    更新 help

--- scripts/help.js
+++ scripts/help.js
@@ -1,10 +1,7 @@
 module.exports = function(bot) {
   bot.hear(/你會做什麼/, (res) => {
     const cmd = [
-      '飲料菜單 店名(目前有: 五十嵐, 珍煮丹, 迷客夏',
-      '123',
-      '444',
-      '12t'
+      '飲料菜單 店名(目前有: 五十嵐, 珍煮丹, 迷客夏)'
     ]
 
     cmd.forEach((c) => {
